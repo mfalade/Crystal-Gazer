10fa694af6062a11483225ba94a673e022ca9193
Toni Solarin-Sodara <toni.edward@outlook.com>
2016-11-27 14:49:11 +0100

    add extra metrics

--- crystal_gazer/app.py
+++ crystal_gazer/app.py
@@ -28,14 +28,34 @@ def get_prediction():
     verdict, csv_data = crystal_gazer.get_prediction_for_data(csv_file.stream)
     results = verdict.values.tolist()
     correct = 0
+    positives = 0
+    negatives = 0
+    false_negatives = 0
+    false_positives = 0
+
     for x in range(0, len(results)):
-        if csv_data["Bootcamp"][x] == int(round(results[x][3])):
-            correct = correct + 1
+        if csv_data["Bootcamp"][x] == 1:
+            positives += 1
+        else:
+            negatives += 1
+
+        result = int(round(results[x][3]))
+        if csv_data["Bootcamp"][x] == result:
+            correct += 1
+        else:
+            if result == 1:
+                false_positives += 1
+            else:
+                false_negatives += 1
 
     accuracy = (float(correct) / float(len(results))) * 100
+    false_negatives = (float(false_negatives) / float(negatives)) * 100
+    false_positives = (float(false_positives) / float(positives)) * 100
     return render_template(
         "result.html",
         file_name=file_name,
         results=results,
-        accuracy=round(accuracy, 2)
+        accuracy=round(accuracy, 2),
+        false_negatives=round(false_negatives, 2),
+        false_positives=round(false_positives, 2)
     )
